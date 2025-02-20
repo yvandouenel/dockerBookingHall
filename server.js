0b91ef74d581ab8365c9bea88e820c40e8544a7f
yvandouenel <yvandouenel@gmail.com>
2025-02-20 22:57:41 +0100

    server.js up to inlcude env variables.

--- server.js
+++ server.js
@@ -8,23 +8,30 @@ dotenv.config();
 
 const app = express();
 
-// Configuration CORS détaillée
+// Configuration CORS selon l'environnement
 const corsOptions = {
     origin: function (origin, callback) {
-        // En développement, on accepte toutes les origines
-        callback(null, true);
-        // En production, on peut (doit) spécifier les domaines autorisés :
-        // const allowedOrigins = ['http://votre-frontend.com'];
-        // if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        //     callback(null, true);
-        // } else {
-        //     callback(new Error('Not allowed by CORS'));
-        // }
+        if (process.env.NODE_ENV === 'development') {
+            // En développement, accepter toutes les origines
+            callback(null, true);
+        } else {
+            // En production, vérifier l'origine
+            const allowedOrigin = process.env.ALLOWED_ORIGIN;
+            
+            // origin peut être undefined pour les requêtes du même serveur
+            if (!origin || origin === allowedOrigin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        }
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
+
+app.use(cors(corsOptions));
 
 
 app.use(cors(corsOptions));
